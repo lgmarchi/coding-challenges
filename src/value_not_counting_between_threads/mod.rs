fa8e6619c807c93be66143792151da6c212e82d3
lgmarchi <lucasgmarchi@gmail.com>
2025-02-12 16:30:39 -0300

    feat: Add benchmarking for thread-safe counter solutions and update Cargo.toml

--- src/value_not_counting_between_threads/mod.rs
+++ src/value_not_counting_between_threads/mod.rs
@@ -1,6 +1,5 @@
 use std::{sync::{atomic::{AtomicUsize, Ordering}, Arc, Mutex, RwLock}, thread};
 
-
 pub fn arc_mutex_solution() -> usize {
     let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
@@ -20,32 +19,8 @@ pub fn arc_mutex_solution() -> usize {
         handle.join().unwrap();
     }
 
-    println!("Final counter value: {}", *counter.lock().unwrap());
+    // println!("Final counter value: {}", *counter.lock().unwrap());
     *counter.clone().lock().unwrap()
-}
-
-pub fn arc_rwlock_solution() -> usize {
-    let counter = Arc::new(RwLock::new(0));
-    let mut handles = vec![];
-
-    for _ in 0..10 {
-        let counter = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            for _ in 0..100 {
-                let mut num = counter.write().unwrap();
-                *num += 1;
-            }
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    println!("Final counter value: {}", *counter.try_read().unwrap());
-    let final_counter_value = *counter.read().unwrap();
-    final_counter_value
 }
 
 // This does not work for complex data like Vec or String
@@ -67,9 +42,35 @@ pub fn arc_atomic_usize_solution() -> usize {
         handle.join().unwrap();
     }
 
-    println!("Final counter value: {}", counter.load(Ordering::Relaxed));
+    // println!("Final counter value: {}", counter.load(Ordering::Relaxed));
     counter.load(Ordering::Relaxed)
 }
+
+
+pub fn arc_rwlock_solution() -> usize {
+    let counter = Arc::new(RwLock::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            for _ in 0..100 {
+                let mut num = counter.write().unwrap();
+                *num += 1;
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // println!("Final counter value: {}", *counter.try_read().unwrap());
+    let final_counter_value = *counter.read().unwrap();
+    final_counter_value
+}
+
 
 #[cfg(test)]
 mod test {
@@ -78,7 +79,7 @@ mod test {
     #[test]
     fn basic() {
         assert_eq!(arc_mutex_solution(), 1000);
-        assert_eq!(arc_rwlock_solution(), 1000);
         assert_eq!(arc_atomic_usize_solution(), 1000);
+        assert_eq!(arc_rwlock_solution(), 1000);
     }
 }
