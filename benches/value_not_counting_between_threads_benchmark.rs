1f614ca656044bedd289fcedd025b7fbc48be2a5
lgmarchi <lucasgmarchi@gmail.com>
2025-04-01 14:46:21 -0300

    feat: Add iterator exercise implementation and update benchmarks

--- benches/value_not_counting_between_threads_benchmark.rs
+++ benches/value_not_counting_between_threads_benchmark.rs
@@ -1,15 +1,15 @@
-
 extern crate coding_challenges;
 
-use coding_challenges::value_not_counting_between_threads::{arc_atomic_usize_solution, arc_mutex_solution, arc_rwlock_solution};
+use coding_challenges::value_not_counting_between_threads::{
+    arc_atomic_usize_solution, arc_mutex_solution, arc_rwlock_solution,
+};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 const SAMPLE_SIZE: usize = 10000;
 
-
 fn benchmark_arc_mutex(c: &mut Criterion) {
     let mut group = c.benchmark_group("Arc Mutex Benchmark");
-    group.sample_size(SAMPLE_SIZE); 
+    group.sample_size(SAMPLE_SIZE);
     group.bench_function("Arc<Mutex<usize>>", |b| {
         b.iter(|| arc_mutex_solution());
     });
@@ -18,7 +18,7 @@ fn benchmark_arc_mutex(c: &mut Criterion) {
 
 fn benchmark_atomic_usize(c: &mut Criterion) {
     let mut group = c.benchmark_group("Arc AtomicUsize Benchmark");
-    group.sample_size(SAMPLE_SIZE); 
+    group.sample_size(SAMPLE_SIZE);
     group.bench_function("Arc<AtomicUsize>", |b| {
         b.iter(|| arc_atomic_usize_solution());
     });
@@ -27,12 +27,17 @@ fn benchmark_atomic_usize(c: &mut Criterion) {
 
 fn benchmark_rwlock(c: &mut Criterion) {
     let mut group = c.benchmark_group("Arc RwLock Benchmark");
-    group.sample_size(SAMPLE_SIZE); 
+    group.sample_size(SAMPLE_SIZE);
     group.bench_function("Arc<RwLock<usize>>", |b| {
         b.iter(|| arc_rwlock_solution());
     });
     group.finish();
 }
 
-criterion_group!(benches, benchmark_arc_mutex, benchmark_atomic_usize, benchmark_rwlock);
+criterion_group!(
+    benches,
+    benchmark_arc_mutex,
+    benchmark_atomic_usize,
+    benchmark_rwlock
+);
 criterion_main!(benches);
---
+++ src/vec_exercise/mod.rs
@@ -0,0 +1,78 @@
+#[derive(Debug)]
+struct Splitter<'a> {
+    input: &'a str,
+    delimiter: char,
+    start: usize,
+}
+
+impl<'a> Splitter<'a> {
+    fn new(input: &'a str, delimiter: char) -> Self {
+        Splitter {
+            input,
+            delimiter,
+            start: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for Splitter<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start > self.input.len() {
+            return None;
+        }
+
+        if self.start == self.input.len() {
+            if self.input.is_empty() {
+                self.start += 1;
+                return Some("");
+            }
+
+            if self.input.chars().last().unwrap() == self.delimiter {
+                self.start += 1;
+                return Some("");
+            }
+            return None;
+        }
+
+        let end = self.input[self.start..].find(self.delimiter);
+
+        match end {
+            Some(index) => {
+                let result = &self.input[self.start..self.start + index];
+                self.start += index + 1;
+                println!("Result: {:?}", result);
+
+                Some(result)
+            }
+            None => {
+                let result = &self.input[self.start..];
+                self.start = self.input.len();
+                println!("Result: {:?}", result);
+                Some(result)
+            }
+        }
+    }
+}
+
+pub fn run_impl_iterator_exercise_for_collect_method() {
+    let test_case = vec![
+        ("Multiple", "ab,cd,ef", ","),
+        ("Multiple", "ab,cd,ef,", ","),
+        ("Multiple", "ab,cd,ef", ":"),
+        ("Multiple", "", ","),
+        ("Multiple", "asasd-3333-asasdsda-d", "-"),
+    ];
+
+    for (name, input, delimiter) in test_case {
+        let delimiter_char = delimiter.chars().next().unwrap();
+        let expected: Vec<_> = input.split(delimiter_char).collect::<Vec<_>>();
+        let splitter = Splitter::new(input, delimiter_char).collect::<Vec<_>>();
+
+        println!("Test: {}", name);
+        println!("Expected: {:?}", expected);
+        println!("Splitter: {:?}", splitter);
+        assert_eq!(expected, splitter);
+    }
+}
