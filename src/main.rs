6f39acfdc5056a4f32fd3203b95ac269806b5c29
lgmarchi-zyphe <lucas@zyphe.com>
2025-04-08 16:20:13 -0300

    Code when I was studying tokio::select!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,55 @@
-use coding_challenges::vec_exercise::run_impl_iterator_exercise_for_collect_method;
+use chrono::Utc;
 
-fn main() {
-    run_impl_iterator_exercise_for_collect_method();
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main() {
+    println!("Iniciando...");
+    let mut resultado = "".to_string();
+
+    let timeout_duration = Duration::from_secs(1);
+    let timeout2 = sleep(timeout_duration);
+
+    tokio::pin!(timeout2);
+
+    tokio::select! {
+        res = tarefa_rapida() => {
+            println!("Tarefa rápida terminou primeiro!");
+            resultado = res;
+        }
+        res = tarefa_lenta() => {
+            println!("Tarefa lenta terminou primeiro!");
+            resultado = res;
+        }
+        _ = &mut timeout2 => {
+            println!("Timeout atingido!");
+            resultado = "timeout".to_string();
+        }
+    }
+
+    println!("{resultado:?}");
 }
+
+async fn tarefa_rapida() -> String {
+    println!("Tarefa rápida começando...");
+    sleep(Duration::from_secs(2)).await;
+    "Tarefa rápida finalizada.".to_string()
+}
+
+async fn tarefa_lenta() -> String {
+    println!("Tarefa lenta começando...");
+    sleep(Duration::from_secs(5)).await;
+    "Tarefa lenta finalizada.".to_string()
+}
+
+// fn main() {
+//     println!("Initial Time: {}", Utc::now());
+
+//     for i in 0..10_000 {
+//         std::thread::spawn(move || {
+//             std::thread::sleep(std::time::Duration::from_secs(1));
+//             println!("Thread {} terminou", i);
+//         });
+//     }
+//     println!("Final Time: {}", Utc::now());
+// }
